c1cdc3b9f1660f7644ab6f2226695543458404d0
Rehan Ahmad <rehan048686@gmail.com>
2025-07-15 16:44:41 +0500

    fix

--- app/api/enquiry/route.js
+++ app/api/enquiry/route.js
@@ -5,7 +5,7 @@ import connectDB from "../../lib/mongodb"
 import CarEnquiry from "../../models/CarEnquiry"
 import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
