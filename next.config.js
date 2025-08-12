c46d14d45fe67dd2d0f784490645daedd5e9ff80
Rehan Ahmad <rehan048686@gmail.com>
2025-08-12 18:52:40 +0500

    fixes

---
+++ next.config.js
@@ -0,0 +1,78 @@
+// import createNextIntlPlugin from "next-intl/plugin"
+
+// const withNextIntl = createNextIntlPlugin("./app/i18n/request.ts")
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     formats: ["image/webp", "image/avif"],
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "*.r2.dev",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "*.r2.cloudflarestorage.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "petbazar.com.pk",
+//       },
+//       {
+//         protocol: "http",
+//         hostname: "localhost",
+//       },
+//     ],
+//   },
+//   // serverExternalPackages: ["sharp"]
+// }
+
+// export default withNextIntl(nextConfig)
+
+
+// next.config.js (change from import to require)
+
+
+const createNextIntlPlugin = require("next-intl/plugin")
+const withNextIntl = createNextIntlPlugin("./app/i18n/request.ts")
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    formats: ["image/webp", "image/avif"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.r2.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "*.r2.cloudflarestorage.com",
+      },
+      {
+        protocol: "https",
+        hostname: "petbazar.com.pk",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
+  },
+  serverExternalPackages: ["sharp"]
+}
+
+module.exports = withNextIntl(nextConfig)
