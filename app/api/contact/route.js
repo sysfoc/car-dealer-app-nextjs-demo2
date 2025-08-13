676ba46c8741fec20f9a1ac391b1face2783ba1e
Rehan Ahmad <rehan048686@gmail.com>
2025-08-13 11:41:53 +0500

    fix

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -4,7 +4,9 @@ import ContactMessage from "../../models/Contact"
 import nodemailer from "nodemailer"
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
--- app/api/valuation/route.js
+++ app/api/valuation/route.js
@@ -75,7 +75,9 @@ export async function PUT(request) {
 
     try {
       const transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASSWORD,
