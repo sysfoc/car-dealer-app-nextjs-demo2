e4a661b505deb3ea7bce85a7dcb21231cda78f77
Rehan Ahmad <rehan048686@gmail.com>
2025-08-13 11:40:06 +0500

    fix

--- app/api/enquiry/route.js
+++ app/api/enquiry/route.js
@@ -4,7 +4,9 @@ import CarEnquiry from "../../models/CarEnquiry"
 import nodemailer from "nodemailer"
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
